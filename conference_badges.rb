60b809de26e034066131dc3a15157fef33acb646
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-10-15 21:50:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,22 +23,9 @@ end
 
 badge_maker(attendees)
 
-
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
-def badge_maker(attendees)
-  attendees.collect do |attendent|
-  "Hello, my name is #{attendent}."
-  end
-end
-
-badge_maker(attendees)
-
 def assign_rooms(attendees)
-  room = 1 
-  attendees.collect do |attendent|
-    puts "Hello, #{attendent}! You'll be assigned to room #{room}!"
-    room += 1 
+  attendees.each_with_index.map do |attendent, index|
+    puts "Hello, #{attendent}! You'll be assigned to room #{index+1}!"
   end
 end
 =end  
@@ -48,18 +35,21 @@ attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+badge_maker(name)
 
 def batch_badge_creator(attendees)
   attendees.map do |attendee|
     "Hello, my name is #{attendee}."
   end
 end
+batch_badge_creator(attendees)
 
 def assign_rooms(attendees)
   attendees.each_with_index.map do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
+assign_rooms(attendees)
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
@@ -70,6 +60,7 @@ def printer(attendees)
     puts assignment
   end
 end
+printer(attendees)
 
 
 
